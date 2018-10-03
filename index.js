a1953877af71c36133f4423b27e9721fa16972ae
Tan Cheng Khoon <ckhoon@tp.edu.sg>
2018-10-04 04:07:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ server.use(bodyParser.json());
 var routes = require('./routes/routes'); //importing route
 routes(server); //register the route
 
-var port = process.env.port || 8000;
+//var port = process.env.port || 8000;
 
-server.listen((port), function () {
+server.listen((process.env.PORT || 8000), function () {
     console.log("Server is up and listening on port" + port);
 });
