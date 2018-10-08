9b2be939f13b43b8b4cf395909ffa30b61610cfd
ckhoon <ckhoon@tp.edu.sg>
2018-10-09 02:27:24 +0800

    test

--- routes/routes.js
+++ routes/routes.js
@@ -11,21 +11,40 @@ module.exports = function(app) {
   });
 
 	app.post('/',function(req,res){
+		console.log(req.body.queryResult.outputContexts);
 		var reply = 'Default webhook reply, -1';
+		var source = '-1';
 	  switch(req.body.queryResult.intent.displayName){
-	    case 'Pain' :
-	      reply = 'I am having chest pain'
+	    case 'Intro_good' :
+	      reply = 'Nope, I am alone. How is my result?';
+	      source = '0';
 	      break;
-	    case 'Pain.Duration.context' :
-	      reply = "The chest pain starts about 4 days ago"
-	      break
-	    case 'Pain.Area.context' :
-	      reply = "The chest pain is at the upper left area"
-	      break
+	    case 'Intro_sad' :
+	      reply = 'You dont remember? How is my result?';
+	      source = '1';
+	      break;
+	    case 'Intro_shock' :
+	      reply = 'I am shocked. How is my result?';
+	      source = '2';
+	      break;
+	    case 'Result_good' :
+	      reply = 'I was worried about my cancer';
+	      source = '0';
+	      break;
+	    case 'Result_sad' :
+	      reply = 'I don\'t know. I am nervous';
+	      source = '1';
+	      break;
+	    case 'Result_shock' :
+	      reply = 'I am shocked.';
+	      source = '2';
+	      break;
 	    default :
 	      reply = "Default webhook reply";
+	      source = '-1';
 	  }
-	  res.json({'fulfillmentText': reply})
+	  res.json({'fulfillmentText': reply,
+							'source': source});
     //res.send('We are happy to see you using Chat Bot Webhook @ post');
   });
 
